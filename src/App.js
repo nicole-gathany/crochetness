494f7cdec59a4aa92332327ba9fd8ea15ab08dd1
Nicole Gathany <nicolegathany@Nicoles-MBP.attlocal.net>
2020-03-22 21:41:28 -0400

    first commit and i added basic components

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import UserView from "./components/UserView";
+import AdminView from "./components/AdminView";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleUserButton = this.handleUserButton.bind(this);
+    this.handleAdminButton = this.handleAdminButton.bind(this);
+    this.state = { isAdmin: false };
+  }
+
+  handleUserButton() {
+    console.log("User View Button works in theory")
+    this.setState({ isAdmin: false });
+  }
+
+  handleAdminButton() {
+    console.log("Admin View Button works in theory")
+    this.setState({ isAdmin: true });
+  }
+  render() {
+    const isAdmin = this.state.isAdmin;
+    let view;
+    if (isAdmin === false) {
+      view = <UserView></UserView>;
+    } else if (isAdmin === true) {
+      view = <AdminView></AdminView>;
+    }
+
+    return (
+      <div className="App">
+        <header>
+          <button onClick={this.handleUserButton}>User View</button>
+          <button onClick={this.handleAdminButton}>Admin View</button>
+          {view}
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/UserView.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { ReactComponent } from '*.svg';
+
+
+//things one should be able to do on the UserView:
+
+class UserView extends React.Component {
+render(){
+    return <div>
+        <h1>This is the User View</h1>
+    </div>
+}
+}
+
+export default UserView;
