be51f6c32a979835e355888e364032c973573869
Nicole Gathany <nicole.gathany@gmail.com>
2020-05-27 18:57:40 -0400

    added to store page to find data and i realize i probably need a backend at this point

--- src/components/UserViews/Shopping/StorePage.js
+++ src/components/UserViews/Shopping/StorePage.js
@@ -24,35 +24,40 @@ class StorePage extends React.Component {
         outfit_type: "sweater",
         source: BlueSweater,
         main_color: "blue",
-        description: "great, big blue sweater for the winter"
+        description: "great, big blue sweater for the winter",
+        clicks: 0
       },
       {
         outfit: "black dress",
         outfit_type: "dress",
         source: BlackDress,
         main_color: "black",
-        description: "great dress for coachella (or coachella inside rn)"
+        description: "great dress for coachella (or coachella inside rn)",
+        clicks: 0
       },
       {
         outfit: "green and black two piece",
         outfit_type: "two piece",
         source: GreenBlack,
         main_color: ["green", "black"],
-        description: "two piece for fun"
+        description: "two piece for fun",
+        clicks:0
       },
       {
         outfit: "pink and blue two piece",
         outfit_type: "two piece",
         source: PinkBlue,
         main_color: "blue",
-        description: "great, big blue sweater for the winter"
+        description: "great, big blue sweater for the winter",
+        clicks: 0 
       },
       {
         outfit: "green skirt",
         outfit_type: "skirt",
         source: GreenSkirt,
         main_color: "green",
-        description: "green skirt"
+        description: "green skirt",
+        clicks: 0
       },
       {
         outfit: "red dress",
@@ -60,26 +65,42 @@ class StorePage extends React.Component {
         source: RedDress,
         main_color: "red",
         description:
-          "nice red dress for your next #norushchallenge or whatever tiktok challenge you do"
+          "nice red dress for your next #norushchallenge or whatever tiktok challenge you do",
+        clicks: 0
       },
       {
         outfit: "magenta dress",
         outfit_type: "dress",
         source: MagentaDress,
         main_color: "magenta",
-        description: "magenta dress so you can do the lord's werk"
+        description: "magenta dress so you can do the lord's werk",
+        clicks: 0
       }
     ];
     this.state = {isProduct: false}
     this.toProductPage = this.toProductPage.bind(this);
+    this.toClicksCounter = this.toProductPage.bind(this);
   }
 
-  toProductPage(){
+  toProductPage(e, value){
     //filter so that there is only one product
     //and shows the product page 
+    e.preventDefault();
+    value.clicks++;
+    console.log(value.clicks)
+    console.log(value.source)
     this.setState({isProduct: true})
   }
 
+  clicksCounter(e, value){
+    //this.products is the array
+    e.preventDefault();
+    console.log(value);
+    value.clicks++;
+    console.log(value.clicks)
+  }
+
+  //i think at this point, i really need a backend
   render() {
     const isProduct = this.state.isProduct;
     let items = this.products.map(product => (
@@ -87,8 +108,9 @@ class StorePage extends React.Component {
         <div className="grid-container">
           <img src={product.source} className="grid-item"></img>
         </div>
-        <button onClick={this.toProductPage}>View this item</button>
-        <button onClick={this.toProductPage}>Add to Cart</button>
+        <button onClick={e => this.clicksCounter(e, product)}>test button</button>
+        <button onClick={e => this.toProductPage(e, product)}>View this item</button>
+        <button onClick={e =>this.toProductPage(e, product)}>Add to Cart</button>
         <button className="favorite-button">♡</button>
       </div>
      
@@ -101,11 +123,13 @@ class StorePage extends React.Component {
     let view=[];
         if(isProduct===false){
             view.push(items);
+            console.log(view)
         } else if(isProduct===true){
-          view.pop()
+          
             view.push(<ProductPage></ProductPage>)
+            console.log(view)
         }
-
+  
 
     return (
       <div className="store-page">
